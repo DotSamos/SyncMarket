0d613f41433b9bac48760b63ca85fc33dd47ef6b
SamosMC <thesamosmc@gmail.com>
2021-12-27 08:12:22 -0300

    Adicionado logo marca e direitos ao plugin

--- src/SyncMarket/SyncMarketPlugin.php
+++ src/SyncMarket/SyncMarketPlugin.php
@@ -11,6 +11,18 @@ use SyncMarket\Queue\Queue;
 use SyncMarket\Task\UpdateSystemTask;
 use SyncMarket\Utils\Bonus;
 
+/**
+ *  ____                           __  __                  _             _   
+ * / ___|   _   _   _ __     ___  |  \/  |   __ _   _ __  | | __   ___  | |_ 
+ * \___ \  | | | | | '_ \   / __| | |\/| |  / _` | | '__| | |/ /  / _ \ | __|
+ *  ___) | | |_| | | | | | | (__  | |  | | | (_| | | |    |   <  |  __/ | |_ 
+ * |____/   \__, | |_| |_|  \___| |_|  |_|  \__,_| |_|    |_|\_\  \___|  \__|
+ *          |___/   
+ * 
+ * @author @SamosMC
+ * @link https://github.com/SamosMC/SyncMarket
+ * @copyright 2021 SyncMarket ltda. CNPJ: 44.466.665/0001-60
+ */
 class SyncMarketPlugin extends PluginBase {
 
     /** @var SyncMarketPlugin */
